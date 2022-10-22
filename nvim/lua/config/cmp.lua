4e68de3119520f7c6dce57eaa35159b31126dbc4
sicojuy <ziqianliao@gmail.com>
2022-10-22 17:10:32 +0800

    update

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -38,8 +38,7 @@ local lsp_flags = {
 	debounce_text_changes = 150,
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require("lspconfig")
 
