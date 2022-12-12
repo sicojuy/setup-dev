3d46d12ff29d9b43df9d1ada54808c1f8e6ab740
sicojuy <ziqianliao@gmail.com>
2022-12-12 10:37:37 +0800

    disable auto window width

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -1,135 +1,147 @@
 vim.cmd("packadd packer.nvim")
 
 return require("packer").startup(function()
-    -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    -- Speed up loading Lua modules in Neovim to improve startup time.
-    use("lewis6991/impatient.nvim")
+	-- Speed up loading Lua modules in Neovim to improve startup time.
+	use("lewis6991/impatient.nvim")
 
-    -- Theme
-    -- use("ellisonleao/gruvbox.nvim")
-    use("morhetz/gruvbox")
-    -- use("projekt0n/github-nvim-theme")
+	-- Theme
+	-- use("ellisonleao/gruvbox.nvim")
+	use("morhetz/gruvbox")
+	-- use("projekt0n/github-nvim-theme")
 
-    -- Status line
-    use("vim-airline/vim-airline")
-    use("vim-airline/vim-airline-themes")
+	-- Status line
+	use("vim-airline/vim-airline")
+	use("vim-airline/vim-airline-themes")
 
-    -- Show how many search matchs
-    use("google/vim-searchindex")
+	-- Show how many search matchs
+	use("google/vim-searchindex")
 
-    -- Intelligently reopen files at your last edit position.
-    use("farmergreg/vim-lastplace")
+	-- Intelligently reopen files at your last edit position.
+	use("farmergreg/vim-lastplace")
 
-    -- Color codes rendering
-    use({
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup()
-        end
-    })
+	-- Color codes rendering
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	})
 
-    -- align
-    use("junegunn/vim-easy-align")
+	-- align
+	use("junegunn/vim-easy-align")
 
-    -- hop anywhere
-    use({
-        "phaazon/hop.nvim",
-        branch = "v2", -- optional but strongly recommended
-        config = function()
-            require("hop").setup()
-        end
-    })
+	-- hop anywhere
+	use({
+		"phaazon/hop.nvim",
+		branch = "v2", -- optional but strongly recommended
+		config = function()
+			require("hop").setup()
+		end,
+	})
 
-    -- thrift
-    use("solarnz/thrift.vim")
+	-- thrift
+	use("solarnz/thrift.vim")
 
-    -- vimwiki
-    use("vimwiki/vimwiki")
+	-- vimwiki
+	use("vimwiki/vimwiki")
 
-    -- Git decorations
-    use("lewis6991/gitsigns.nvim")
+	-- Git decorations
+	use("lewis6991/gitsigns.nvim")
 
-    -- Git conflict marker
-    use("rhysd/conflict-marker.vim")
+	-- Git conflict marker
+	use("rhysd/conflict-marker.vim")
 
-    -- Git premier
-    use("tpope/vim-fugitive")
+	-- Git premier
+	use("tpope/vim-fugitive")
 
-    -- Terminal
-    use("akinsho/toggleterm.nvim")
+	-- Terminal
+	use("akinsho/toggleterm.nvim")
 
-    -- markdown
-    use({
-        "iamcco/markdown-preview.nvim",
-        requires = {
-            "iamcco/mathjax-support-for-mkdp",
-        },
-        run = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    })
+	-- markdown
+	use({
+		"iamcco/markdown-preview.nvim",
+		requires = {
+			"iamcco/mathjax-support-for-mkdp",
+		},
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
 
-    -- nvim-tree
-    use("kyazdani42/nvim-tree.lua")
+	-- nvim-tree
+	use("kyazdani42/nvim-tree.lua")
 
-    -- window size
-    use({ "anuvyklack/windows.nvim",
-        requires = "anuvyklack/middleclass",
-        config = function()
-            require('windows').setup()
-        end
-    })
+	-- window size
+	use({
+		"anuvyklack/windows.nvim",
+		requires = "anuvyklack/middleclass",
+		config = function()
+			require("windows").setup({
+				autowidth = {
+					enable = false,
+				},
+				ignore = {
+					buftype = { "quickfix" },
+					filetype = { "NvimTree", "neo-tree", "undotree", "gundo" },
+				},
+				animation = {
+					enable = false,
+				},
+			})
+		end,
+	})
 
-    -- treesitter
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-    })
+	-- treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
-    -- Symbols outline
-    use({
-        "simrat39/symbols-outline.nvim",
-        config = function()
-            require("symbols-outline").setup()
-        end
-    })
+	-- Symbols outline
+	use({
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	})
 
-    -- Fuzzy picker
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = { { "nvim-lua/plenary.nvim" } },
-    })
+	-- Fuzzy picker
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    -- Improve sorting performance
-    use({
-        "nvim-telescope/telescope-fzf-native.nvim",
-        run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-    })
+	-- Improve sorting performance
+	use({
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	})
 
-    -- lsp client config
-    use("neovim/nvim-lspconfig")
-    use({
-        "hrsh7th/nvim-cmp", -- completion
-        requires = {
-            "hrsh7th/cmp-nvim-lsp", -- cmp lsp
-            "hrsh7th/cmp-buffer", -- cmp buffer
-            "hrsh7th/cmp-path", -- cmp path
-            "hrsh7th/cmp-cmdline", -- cmp cmdline
-        },
-    })
+	-- lsp client config
+	use("neovim/nvim-lspconfig")
+	use({
+		"hrsh7th/nvim-cmp", -- completion
+		requires = {
+			"hrsh7th/cmp-nvim-lsp", -- cmp lsp
+			"hrsh7th/cmp-buffer", -- cmp buffer
+			"hrsh7th/cmp-path", -- cmp path
+			"hrsh7th/cmp-cmdline", -- cmp cmdline
+		},
+	})
 
-    -- vsnip
-    use("hrsh7th/cmp-vsnip")
-    use("hrsh7th/vim-vsnip")
+	-- vsnip
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
 
-    -- show function signature
-    use("ray-x/lsp_signature.nvim")
+	-- show function signature
+	use("ray-x/lsp_signature.nvim")
 
-    -- go
-    use("fatih/vim-go")
+	-- go
+	use("fatih/vim-go")
 
-    -- rust
-    use("rust-lang/rust.vim")
+	-- rust
+	use("rust-lang/rust.vim")
 end)
