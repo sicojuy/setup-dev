e3a7ca609a3f4cf4a8abb31806ce342b2d1f4283
sicojuy <ziqianliao@gmail.com>
2022-12-14 23:22:02 +0800

    update

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -11,8 +11,8 @@ local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
 -- I don't want to search in the `.git` directory.
 table.insert(vimgrep_arguments, "--glob=!.git/*")
-table.insert(vimgrep_arguments, "--glob=!*/mock/*")
-table.insert(vimgrep_arguments, "--glob=!*/mocks/*")
+table.insert(vimgrep_arguments, "--glob=!mock/*")
+table.insert(vimgrep_arguments, "--glob=!mocks/*")
 table.insert(vimgrep_arguments, "--glob=!kitex_gen/*")
 table.insert(vimgrep_arguments, "--glob=!output/*")
 table.insert(vimgrep_arguments, "--glob=!*.o")
@@ -22,49 +22,49 @@ table.insert(vimgrep_arguments, "--glob=!*_test.go")
 
 -- ignore large file
 local new_maker = function(filepath, bufnr, opts)
-    opts = opts or {}
+	opts = opts or {}
 
-    filepath = vim.fn.expand(filepath)
-    vim.loop.fs_stat(filepath, function(_, stat)
-        if not stat then
-            return
-        end
-        if stat.size > 100000 then
-            return
-        else
-            previewers.buffer_previewer_maker(filepath, bufnr, opts)
-        end
-    end)
+	filepath = vim.fn.expand(filepath)
+	vim.loop.fs_stat(filepath, function(_, stat)
+		if not stat then
+			return
+		end
+		if stat.size > 100000 then
+			return
+		else
+			previewers.buffer_previewer_maker(filepath, bufnr, opts)
+		end
+	end)
 end
 
 require("telescope").setup({
-    defaults = require("telescope.themes").get_ivy({
-        vimgrep_arguments = vimgrep_arguments,
-        buffer_previewer_maker = new_maker,
-        mappings = {
-            i = {
-                --['<esc>'] = actions.close,
-            },
-        },
-    }),
-    pickers = {
-        find_files = {
-            find_command = {
-                "rg",
-                "--files",
-                "--glob=!.git/*",
-                "--glob=!*/mock/*",
-                "--glob=!*/mocks/*",
-                "--glob=!kitex_gen/*",
-                "--glob=!output/*",
-                "--glob=!*.o",
-                "--glob=!*.so",
-                "--glob=!*.py[co]",
-                "--glob=!*_test.go",
-            },
-        },
-    },
-    extensions = {},
+	defaults = require("telescope.themes").get_ivy({
+		vimgrep_arguments = vimgrep_arguments,
+		buffer_previewer_maker = new_maker,
+		mappings = {
+			i = {
+				--['<esc>'] = actions.close,
+			},
+		},
+	}),
+	pickers = {
+		find_files = {
+			find_command = {
+				"rg",
+				"--files",
+				"--glob=!.git/*",
+				"--glob=!mock/*",
+				"--glob=!mocks/*",
+				"--glob=!kitex_gen/*",
+				"--glob=!output/*",
+				"--glob=!*.o",
+				"--glob=!*.so",
+				"--glob=!*.py[co]",
+				"--glob=!*_test.go",
+			},
+		},
+	},
+	extensions = {},
 })
 
 require("telescope").load_extension("fzf")
